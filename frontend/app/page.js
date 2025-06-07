07be1f05a00d88b0d5cdb10374faec1a5f56ec07
Deeksha <deeksha@noseberry.com>
2025-06-07 12:06:46 +0530

    setup

---
+++ frontend/app/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <p className="text-red-500">hello</p>
+      </div>
+    </main>
+  );
+}
